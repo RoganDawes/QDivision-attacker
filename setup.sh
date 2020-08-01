c5f4524f1b0a493397506d0e264e18e615858c2f
Rogan Dawes <rogan@dawes.za.net>
2020-08-02 01:19:58 +0200

    Change to the right directory before running

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+cd $( cd $(dirname $0) && pwd)
 
 printf "lan\tplatform-1c1b000.usb-usb-0:1:1.0\nwan\tplatform-1c30000.ethernet\nwlan\tplatform-1c10000.mmc\n" | while read iface path; do
 cat << EOF > /etc/systemd/network/10-$iface.link
@@ -10,12 +11,7 @@ Name=$iface
 EOF
 done
 
-cat << EOF >> /etc/NetworkManager/NetworkManager.conf
-
-[keyfile]
-unmanaged-devices=interface-name:wan,interface-name:lan,interface-name:wlan0,interface-name:wlan1
-
-EOF
+cp NetworkManager.conf /etc/NetworkManager/NetworkManager.conf
 
 echo mac80211_hwsim > /etc/modules
 echo br_nefilter >> /etc/modules
@@ -23,6 +19,8 @@ echo br_nefilter >> /etc/modules
 # systemctl disable serial-getty@ttyGS0.service
 
 apt update
-apt install -y python3-setuptools python3-dev libpcap-dev
+DEBIAN_FRONTEND=noninteractive apt install -y python3-setuptools python3-dev libpcap-dev
+DEBIAN_FRONTEND=noninteractive apt install -y iftop dnsmasq tcpdump ebtables redsocks macchanger
+DEBIAN_FRONTEND=noninteractive apt install -y python3-pypcap python3-dpkt python3-pyroute2 python3-iptables python3-cffi
 
 cp pitm-sysctl.conf /etc/sysctl.d/
